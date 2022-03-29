2f60bc9b0c64f7be44f989f83607224fb6493c1d
Jingxian Chai <jingxianchai01@gmail.com>
2022-03-28 23:21:53 -0500

    added on-demand ISR

--- env.d.ts
+++ env.d.ts
@@ -2,5 +2,6 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     NEXT_PUBLIC_SUPABASE_URL: string
     SUPABASE_SERVICE_ROLE_KEY: string
+    REVALIDATE_SECRET: string
   }
 }
---
+++ pages/api/revalidate.ts
@@ -0,0 +1,27 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next"
+
+type Data = {
+  message?: string
+  revalidated?: boolean
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // Check for secret to confirm this is a valid request
+  if (req.query.secret !== process.env.REVALIDATE_SECRET) {
+    return res.status(401).json({ message: "Invalid token" })
+  }
+
+  try {
+    // Regenerate our index route showing the images
+    await res.unstable_revalidate("/")
+    return res.json({ revalidated: true })
+  } catch (err) {
+    // If there was an error, Next.js will continue
+    // to show the last successfully generated page
+    return res.status(500).send({ message: "Error revalidating" })
+  }
+}
